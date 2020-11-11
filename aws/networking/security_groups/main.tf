8940d05f5d332f6533422d1c2ffed47fa884dd50
NadavOps <ubuntu@LetsGo-2.localdomain>
2020-11-11 03:12:19 +0200

    aws sg module

---
+++ aws/networking/security_groups/main.tf
@@ -0,0 +1,34 @@
+resource "aws_security_group" "sg" {
+  count       = var.sg_create_enabled == true ? length(var.security_groups_list) : 0
+  name        = var.security_groups_list[count.index][0]
+  description = var.security_groups_list[count.index][1]
+  vpc_id      = var.vpc_id
+
+tags = var.tags
+}
+
+locals {
+  map_sg_name_2_id = { for sg in aws_security_group.sg : sg.name => sg.id }
+}
+
+resource "aws_security_group_rule" "assign_rule_to_sg" {
+  count             = length(var.simple_rules)
+  type              = lookup(var.simple_rules[count.index], "type", null)
+  from_port         = lookup(var.simple_rules[count.index], "from_port", null)
+  to_port           = lookup(var.simple_rules[count.index], "to_port", null)
+  protocol          = lookup(var.simple_rules[count.index], "protocol", null)
+  cidr_blocks       = lookup(var.simple_rules[count.index], "cidr_blocks", null)
+  description       = lookup(var.simple_rules[count.index], "description", null)
+  security_group_id = lookup(local.map_sg_name_2_id, lookup(var.simple_rules[count.index], "sg_name", null), null)
+}
+
+resource "aws_security_group_rule" "assign_sg_to_sg" {
+  count             = length(var.source_sg_rules)
+  type              = lookup(var.source_sg_rules[count.index], "type", null)
+  from_port         = lookup(var.source_sg_rules[count.index], "from_port", null)
+  to_port           = lookup(var.source_sg_rules[count.index], "to_port", null)
+  protocol          = lookup(var.source_sg_rules[count.index], "protocol", null)
+  source_security_group_id = lookup(local.map_sg_name_2_id, lookup(var.source_sg_rules[count.index], "source_sg_name", null), null)
+    description       = lookup(var.source_sg_rules[count.index], "description", null)
+      security_group_id = lookup(local.map_sg_name_2_id, lookup(var.source_sg_rules[count.index], "sg_name", null), null)
+}
---
+++ aws/networking/security_groups/variables.tf
@@ -0,0 +1,42 @@
+### Required vars
+variable "security_groups_list" {
+  # example [ ["sg1", "description1"], ["sg2", "description2"]]
+  description = "A list of security groups names and descriptions, controls the amount of groups to be created"
+  type        = list(list(string))
+}
+
+variable "vpc_id" {
+  description = "The VPC ID in which the security group will be created"
+  type        = string
+}
+
+### Optional vars
+variable "simple_rules" {
+  # example: 
+  # { type = "ingress/egress", from_port = 1, to_port = 1, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"],
+  # description = "", sg_name = "name_from => security_groups_list" }
+  description = "A list of rule maps"
+  type        = list(any)
+  default = []
+}
+
+variable "source_sg_rules" {
+  # example: 
+  # { type = "ingress/egress", from_port = 1, to_port = 1, protocol = "tcp", description = "",
+  # source_sg_name = "name_from => security_groups_list", sg_name = "name_from => security_groups_list" }
+  description = "A list of rule maps, assigning security group as the source"
+  type        = list(map(string))
+  default = []
+}
+
+variable "sg_create_enabled" {
+  description = "true by default to enable security groups creation"
+  type        = bool
+  default = true
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to the ec2 instance"
+  type        = map(string)
+  default     = {}
+}
